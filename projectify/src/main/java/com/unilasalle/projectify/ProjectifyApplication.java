e40056ae812c5e3fdfdf109a26fb6083890e9db0
João Pedro Alves Sorrentino <86813673+joaosorrentino@users.noreply.github.com>
2025-03-18 21:09:45 -0300

    first commit

---
+++ projectify/src/main/java/com/unilasalle/projectify/ProjectifyApplication.java
@@ -0,0 +1,13 @@
+package com.unilasalle.projectify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectifyApplication.class, args);
+	}
+
+}
